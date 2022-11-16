665090174840c116fd1c27fd0382268959c6b3fa
Valentin Korneev <v.korneef@gmail.com>
2022-11-16 18:38:04 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { item } from './item';
+import ShopItemFunc from './ShopItemFunc';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ShopItemClass item={item} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
